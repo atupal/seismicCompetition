94f84710422e9fa4aa08285709110010da676cd5
yan <6744617@qq.com>
2014-11-16 17:17:39 -0600

    add segymat

--- rtm/mex_ICC_O3.sh
+++ rtm/mex_ICC_O3.sh
@@ -13,6 +13,6 @@ option="$option -pthread"
 ldoption="-fopenmp"
 
 #icc -O3 rtm2d_fm2d_mex.core.cpp -o rtm2d_fm2d_mex.core -openmp -simd -xAVX -vec-report || exit 1
-g++ -fopenmp -O3 rtm2d_fm2d_mex.core.cpp -o rtm2d_fm2d_mex.core || exit 1
+clang++ -fopenmp -O3 rtm2d_fm2d_mex.core.cpp -o rtm2d_fm2d_mex.core || exit 1
 
 mex CXX="g++" CXXFLAGS="-O2 $option" LDFLAGS="$ldoption" rtm2d_fm2d_mex.cpp
