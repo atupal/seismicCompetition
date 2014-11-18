38190da6f65ba7302b119e6d93d0fbec1e338ba4
atupal <atupalykl@gmail.com>
2014-11-18 15:47:16 +0000

    update model of sc14

---
+++ support/LoadHessVTIParams.m
@@ -0,0 +1,10 @@
+%% Define Velocity model parameters
+dz = 20;                    % depth spacing (ft)
+dx = 20;                    % horizontal spacing (ft)
+
+% windowed velocity model is receiver area covered
+ns = 1206;                  % number of shots
+nr = 220;                   % number of receivers
+
+dt = 8.599965497466239e-04; % sampling time (s)
+nt = 2895;                  % trace size
---
+++ support/LoadSaltToothParams.m
@@ -0,0 +1,10 @@
+%% Define Velocity model parameters
+dz = 06.25;                 % depth spacing (m)
+dx = 12.25;                 % horizontal spacing (m)
+
+% windowed velocity model is receiver area covered
+ns = 1079;                  % number of shots
+nr = 220;                   % number of receivers
+
+dt = 8.303706981574801e-04; % sampling time (s)
+nt = 5100;                  % trace size
