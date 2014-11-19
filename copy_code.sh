6015bd3b15745db3cc3b5dfb9fa64d934a9811fb
atupal <atupalykl@gmail.com>
2014-11-19 18:01:16 +0000

    update

--- copy_code.sh
+++ copy_code.sh
@@ -2,4 +2,5 @@
 
 #rsync -avzhe "ssh" ./ gpuusr@202.114.10.172:/home/gpuusr/atupal/seismicCompetition --exclude '.git' --exclude 'codegen'
 
-rsync -avzhe "ssh" ./ sc14@10.0.4.201:/public/sc14/matlab/seismicCompetition-git --exclude '.git' --exclude 'codegen'
+rsync -avzhe "ssh" ./ sc14@10.0.4.201:/public/sc14/matlab/seismicCompetition-git --exclude '.git' --exclude 'codegen' \
+  --exclude 'results'
--- set_sem.sh
+++ set_sem.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 ssh cpu1 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
 ssh cpu3 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
+ssh cpu4 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
+ssh gpu1 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
+ssh gpu2 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
+ssh gpu3 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
+ssh gpu4 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
+ssh gpu5 "cd /public/sc14/matlab/seismicCompetition-git/rtm && ./sem && ./sem 11"
